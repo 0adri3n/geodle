e1f97d1de75165c56e3fbe21a89e73769d235be9
0adri3n <akiratrinity@protonmail.com>
2024-05-03 15:24:49 +0200

    adding datas & test with it

--- server.py
+++ server.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask import render_template, url_for, request, flash, redirect
 
+import json
+
 app = Flask(__name__)
 app.secret_key = "geodle"
 
 
 @app.route('/')
 def index(): 
-
+    f = open("static/data/data.json", "r")
+    pays = json.loads(f.read())
+    f.close()
+    l = 0
+    # Parcourir chaque pays
+    for country in pays:
+        l+=1
+        print("Nom du pays :", country["name"])
+        print("Capitale :", country["capital"])
+        print("Population :", country["population"])
+        print("Langues :", ", ".join([lang["name"] for lang in country["languages"]]))
+        print("Drapeau :", country["flag"])
+        print("Régions :", country["region"], "-", country["subregion"])
+        print("-----------------------------------------")
+    print(l)
     return render_template('index.html')
 
 if __name__ == '__main__': 
